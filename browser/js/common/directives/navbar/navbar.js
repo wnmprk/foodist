6a0599335092983c76cfb89810e479b28d850588
wnmprk <wonmipark@gmail.com>
2016-05-04 16:53:44 -0400

    rid navbar, fix search

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -6,23 +6,6 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
         templateUrl: 'js/common/directives/navbar/navbar.html',
         link: function (scope) {
             
-            scope.addForm = () => {
-                var formModal = $uibModal.open({
-                    animation: scope.animationsEnabled,
-                    templateUrl: '/js/foods/add-food.html',
-                    controller: () => {
-                        scope.saveFood = (foodObj) => {
-                            console.log('IM IN THE save food func', foodObj)
-                            FoodFactory.add(foodObj)
-                            .then( () => {
-                                // page does not update with new item immediately
-                                formModal.close();
-                            });
-                        }
-                    }
-                })
-            }
-
             scope.user = null;
 
             scope.isLoggedIn = function () {
--- browser/js/foods/foods.js
+++ browser/js/foods/foods.js
@@ -35,5 +35,23 @@ app.controller('FoodCtrl', ($scope, $state, $uibModal, AuthService, FoodFactory,
             console.log(user.name, 'un/liked', food.name);
         })
     }
+    // brokennnnnnnnnn
+    $scope.addForm = () => {
+        var formModal = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: '/js/foods/add-food.html',
+            controller: () => {
+                $scope.saveFood = (foodObj) => {
+                    console.log('IM IN THE save food func', foodObj)
+                    FoodFactory.add(foodObj)
+                    .then( () => {
+                        // page does not update with new item immediately
+                        formModal.close();
+                    });
+                }
+            }
+        })
+    }
+
 
 });
